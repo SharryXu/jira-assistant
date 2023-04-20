b91a3d5ef0eed892e064e185b77a16233c968c3a
Sharry <sharry.xu@outlook.com>
2023-04-20 15:09:25 +0800

    Print error message about failed test.

--- src/jira_assistant/console_script.py
+++ src/jira_assistant/console_script.py
@@ -239,11 +239,10 @@ def _generate_timestamp_filename(prefix: str, extension: str) -> "Path":
 
 def _generate_excel_template(output_file: "Path") -> Optional[Path]:
     try:
+        print(files("jira_assistant.assets").joinpath("excel_definition.json"))
         excel_definition = ExcelDefinition().load(
             files("jira_assistant.assets").joinpath("excel_definition.json").read_text()
         )
-        print("Test")
-        print(output_file.absolute())
         output_to_excel_file(output_file, [], excel_definition)
         return output_file
     except Exception as e:
